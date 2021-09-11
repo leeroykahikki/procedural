68da8ac045aa15cd29954814e454fe5f0fbed638
leeroykahikki <60253601+leeroykahikki@users.noreply.github.com>
2021-09-11 18:13:05 +0300

    Изменил filter

--- block 3/filter/main.cpp
+++ block 3/filter/main.cpp
@@ -14,31 +14,25 @@ int main()
   setlocale(LC_ALL,"rus");
 
   string line;
-  ifstream in("./sort.txt");
+  ifstream in("./filter.txt");
 
   if (in.is_open()) {
     while (getline(in, line)) {
-      cout << line.length() << endl;
-      cout << line[2] << endl;
+      bool isNumber = true;
+
+      for (auto c: line) {
+        if (!isdigit(c)) {
+          isNumber = false;
+        }
+      }
       
+      if (isNumber) {
+        cout << line << endl;
+      }
     }
   }
 
   in.close();
 
   return 0; 
-}
-
-void SortAlgo(int data[], int lenD)
-{
-  int tmp = 0;
-  for(int i = 0;i<lenD;i++){
-    for(int j = (lenD-1);j>=(i+1);j--){
-      if(data[j]<data[j-1]){
-        tmp = data[j];
-        data[j]=data[j-1];
-        data[j-1]=tmp;
-      }
-    }
-  }
 }
